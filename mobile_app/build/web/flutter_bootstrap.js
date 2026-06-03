5598597ef53ee3d1ede491f46b39ea7c79a2fcf6
mumitrol16666-creator <mumitrol16666@gmail.com>
2026-06-03 13:35:16 +0500

    Update web build with real data and no mocks

--- mobile_app/build/web/flutter_bootstrap.js
+++ mobile_app/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "240713398" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "44279580" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
