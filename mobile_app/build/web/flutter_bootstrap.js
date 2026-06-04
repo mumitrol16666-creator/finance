478b9a1443218887230f3c9a23743f536c9300dd
mumitrol16666-creator <mumitrol16666@gmail.com>
2026-06-04 09:28:07 +0500

    feat: Add time picker for daily report and quiet hours start/end in settings

--- mobile_app/build/web/flutter_bootstrap.js
+++ mobile_app/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2385709657" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4254459068" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
